930a188e9aa0af49e3169e7e4b228391b163938b
zbaker94 <zbaker94@gmail.com>
2024-05-24 11:36:05 -0400

    chore: Update dependencies and error handling in weather module

--- weather/__init__.py
+++ weather/__init__.py
@@ -8,10 +8,20 @@ __version__ = "0.1.1"
     SUCCESS,
     IP_ERROR,
     API_ERROR,
-) = range(3)
+    DIR_ERROR, 
+    FILE_ERROR,
+    CONFIG_FILE_ERROR,
+    CONFIG_VALUE_ERROR,
+    LOCATION_ERROR
+) = range(8)
 
 ERRORS = {
     SUCCESS: "Success",
     IP_ERROR: "IP address not found",
     API_ERROR: "API error",
+    DIR_ERROR: "Directory not found",
+    FILE_ERROR: "File not found",
+    LOCATION_ERROR: "Location not found or invalid. ",
+    CONFIG_FILE_ERROR: "Config file not found. have you run 'init' command?",
+    CONFIG_VALUE_ERROR: "Config file does not have a valid api key. have you run 'init' command?",
 }
